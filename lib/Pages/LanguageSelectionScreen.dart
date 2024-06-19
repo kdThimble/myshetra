0d5c95763f8b0988dd236e3bd3d5c90c048b5766
kdThimble <kd@thimble.io>
2024-06-20 00:30:59 +0530

    first commit

---
+++ lib/Pages/LanguageSelectionScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageSelectionPage extends StatefulWidget {
+  const LanguageSelectionPage({super.key});
+
+  @override
+  State<LanguageSelectionPage> createState() => _LanguageSelectionPageState();
+}
+
+class _LanguageSelectionPageState extends State<LanguageSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Pages/SplashScreen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        backgroundColor: Color(0xFFFF2B2B),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('My Shetra',
+                  style: TextStyle(
+                      fontSize: width * 0.12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              SizedBox(height: 10),
+              Text('Some text here',
+                  style: TextStyle(
+                      fontSize: width * 0.09,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white)),
+            ],
+          ),
+        ));
+  }
+}
