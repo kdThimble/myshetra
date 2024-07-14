5bf7e207936dfe6c327afa109f45a3a670029bf6
kdThimble <kd@thimble.io>
2024-07-14 23:55:19 +0530

    internet issue solved

--- lib/Controller/networkController.dart
+++ lib/Controller/networkController.dart
@@ -4,56 +4,117 @@ import 'package:get/get.dart';
 
 class NetworkController extends GetxController {
   final Connectivity _connectivity = Connectivity();
+  var isConnected = true.obs;
 
   @override
   void onInit() {
     super.onInit();
     _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    _checkInitialConnection(); // Check the initial connection status
+  }
+
+  void _checkInitialConnection() async {
+    var connectivityResult = await _connectivity.checkConnectivity();
+    _updateConnectionStatus(connectivityResult);
   }
 
   void _updateConnectionStatus(ConnectivityResult connectivityResult) {
     if (connectivityResult == ConnectivityResult.none) {
-      Get.defaultDialog(
-        barrierDismissible: false,
-        backgroundColor: Colors.white,
-        title: 'No internet',
-        content: Column(
-          children: [
-            SizedBox(height: 20),
-            Image.asset(
-                "assets/images/nointernet-transformed-removebg-preview-transformed.png"),
-            SizedBox(height: 20),
-            Text(
-              'Please Connect to the internet',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      );
-      Get.rawSnackbar(
-          messageText: const Text('PLEASE CONNECT TO THE INTERNET',
-              style: TextStyle(color: Colors.white, fontSize: 14)),
-          isDismissible: false,
-          duration: const Duration(days: 1),
-          backgroundColor: Colors.red[400]!,
-          icon: const Icon(
-            Icons.wifi_off,
-            color: Colors.white,
-            size: 35,
-          ),
-          margin: EdgeInsets.zero,
-          snackStyle: SnackStyle.GROUNDED);
+      isConnected.value = false;
     } else {
-      if (Get.isSnackbarOpen) {
-        Get.closeCurrentSnackbar();
-        Get.closeAllSnackbars();
-      }
-      if (Get.isDialogOpen ?? false) {
-        Get.back(); // Close the dialog
-      }
+      isConnected.value = true;
+    }
+
+    if (!isConnected.value) {
+      // _showNoInternetDialog();
+      _showNoInternetSnackbar();
+    } else {
+      _closeNoInternetDialog();
+      _closeNoInternetSnackbar();
+    }
+  }
+
+  // void _showNoInternetDialog() {
+  //   if (Get.isDialogOpen != true) {
+  //     Get.defaultDialog(
+  //       barrierDismissible: true,
+  //       backgroundColor: Colors.white,
+  //       title: 'No internet',
+  //       content: Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: [
+  //           SizedBox(height: 20),
+  //           Image.asset(
+  //               "assets/images/nointernet-transformed-removebg-preview-transformed.png"),
+  //           SizedBox(height: 20),
+  //           Text(
+  //             'Please Connect to the internet',
+  //             style: TextStyle(
+  //               fontSize: 20,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     );
+  //   }
+}
+
+void _showNoInternetSnackbar() {
+  if (!Get.isSnackbarOpen) {
+    Get.rawSnackbar(
+      borderColor: Colors.grey,
+      borderRadius: 10,
+      messageText: Container(
+        height: Get.height * 0.7,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'No internet Connection',
+                style: TextStyle(
+                    color: Colors.red[400]!,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              Image.asset(
+                  "assets/images/nointernet-transformed-removebg-preview-transformed.png"),
+              SizedBox(height: 20),
+              Text(
+                'Please Connect to the internet',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      isDismissible: false,
+      duration: const Duration(days: 1),
+      backgroundColor: Colors.white,
+      barBlur: 5,
+      margin: EdgeInsets.zero,
+      snackStyle: SnackStyle.FLOATING,
+    );
+  }
+}
+
+void _closeNoInternetDialog() {
+  if (Get.isDialogOpen == true) {
+    Get.back();
+  }
+}
+
+void _closeNoInternetSnackbar() {
+  if (Get.isSnackbarOpen) {
+    try {
+      Get.closeAllSnackbars();
+    } catch (e) {
+      // Handle the case where snackbar is already disposed
     }
   }
 }
